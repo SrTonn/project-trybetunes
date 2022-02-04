72685e9bcb0c730fcd3d9d680301ed61e6e4135e
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-02-04 00:54:18 -0300

    Header estilizado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { getUser } from '../../services/userAPI';
 import Loading from '../Loading';
+import Logo from '../../assets/logo-trybe-tunes.svg';
 import './index.css';
 
 export default class Header extends Component {
@@ -23,9 +25,35 @@ export default class Header extends Component {
   render() {
     const { userName, isLoading } = this.state;
     return (
-      <div data-testid="header-component">
-        {isLoading ? <Loading /> : <p data-testid="header-user-name">{userName}</p> }
-      </div>
+      <header data-testid="header-component">
+        <div className="header-container">
+          <Link to="/">
+            <img src={ Logo } alt="logo-tipo trybetunes" />
+          </Link>
+          {isLoading ? <Loading /> : (
+            <div>
+              <img
+                className="profile-image"
+                src={ `https://github.com/${userName}.png` }
+                alt="user ico"
+              />
+              <span
+                className="header-user-name"
+                data-testid="header-user-name"
+              >
+                {userName}
+              </span>
+            </div>
+          ) }
+        </div>
+        {!isLoading && (
+          <section className="section-links" data-testid="link-to-profile">
+            <NavLink to="/search">Pesquisa</NavLink>
+            <NavLink to="/favorites">Favorita</NavLink>
+            <NavLink to="/profile">Perfil</NavLink>
+          </section>
+        )}
+      </header>
     );
   }
 }
