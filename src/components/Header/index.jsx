100a1f1e6c3b3073c340942a1a62ccdff9f8f45e
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-02-10 09:16:08 -0300

    ajuste na estilizacao do component Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Link, NavLink } from 'react-router-dom';
 import Loading from '../Loading';
 import Logo from '../../assets/logo-trybe-tunes.svg';
-import './index.css';
+import styles from './styles.module.css';
 
 export default class Header extends Component {
   render() {
@@ -11,19 +11,19 @@ export default class Header extends Component {
 
     return (
       <header data-testid="header-component">
-        <div className="header-container">
+        <div className={ styles.HeaderContainer }>
           <Link to="/">
             <img src={ Logo } alt="logo-tipo trybetunes" />
           </Link>
           {isLoading ? <Loading /> : (
-            <div>
+            <div className={ styles.ImgAndUserNameContainer }>
               <img
-                className="profile-image"
+                className={ styles.ProfileImage }
                 src={ `https://github.com/${userName}.png` }
                 alt="user ico"
               />
               <span
-                className="header-user-name"
+                className={ styles.HeaderUserName }
                 data-testid="header-user-name"
               >
                 {userName}
@@ -32,7 +32,7 @@ export default class Header extends Component {
           ) }
         </div>
         {!isLoading && (
-          <section className="section-links">
+          <section className={ styles.SectionLinks }>
             <NavLink to="/search" data-testid="link-to-search">Pesquisa</NavLink>
             <NavLink to="/favorites" data-testid="link-to-favorites">Favorita</NavLink>
             <NavLink to="/profile" data-testid="link-to-profile">Perfil</NavLink>
